c45340863568025a2e0ca6f56f8a3327862a330d
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2022-11-29 15:48:06 +0100

    intro website + header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const nameTitle = document.querySelector('.title');
-const intro = document.querySelector('.title');
+const centerElement = document.querySelector('.center-element');
 const loader = document.querySelector('.hex-element');
+const wrapper = document.querySelector('.start-website');
 
 nameTitle.addEventListener('click', startLoader);
 
 function startLoader(){
-    intro.classList.add('invisible');
+    const loadWebsite = setTimeout(startWebsite, 3000);
+    nameTitle.classList.add('invisible');
     loader.classList.add('visible');
+}
+
+function startWebsite(){
+    centerElement.remove();
+    wrapper.classList.add('wrapper');
 }
