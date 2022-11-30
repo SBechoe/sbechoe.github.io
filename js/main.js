156cae96f2e232e5fc8e05040e304b281a2a7b0a
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2022-11-30 16:29:58 +0100

    Toggle nav working

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@ const nameTitle = document.querySelector('.title');
 const centerElement = document.querySelector('.center-element');
 const loader = document.querySelector('.hex-element');
 const wrapper = document.querySelector('.start-website');
+const nav = document.querySelector('.mobile');
+const burger = document.querySelector('.icon');
+
+let navState = "closed";
 
 nameTitle.addEventListener('click', startLoader);
+burger.addEventListener('click', toggleNav);
 
 function startLoader(){
-    const loadWebsite = setTimeout(startWebsite, 3000);
+    const loadWebsite = setTimeout(startWebsite, 1000);
     nameTitle.classList.add('invisible');
     loader.classList.add('visible');
 }
@@ -14,4 +19,17 @@ function startLoader(){
 function startWebsite(){
     centerElement.remove();
     wrapper.classList.add('wrapper');
+}
+
+function toggleNav(){
+    if(navState == "closed"){
+        nav.classList.remove('invisible');
+        navState = "open";
+    }
+    else if(navState == "open"){
+        nav.classList.remove('visible');
+        nav.classList.add('invisible');
+        navState = "closed";
+    }
+
 }
