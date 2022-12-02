f75aa4ec3847672b483794437f9c352fc6c05cb1
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2022-12-02 23:59:54 +0100

    mobile state working

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@ const centerElement = document.querySelector('.center-element');
 const loader = document.querySelector('.hex-element');
 const wrapper = document.querySelector('.start-website');
 const nav = document.querySelector('.mobile');
+const navItems= document.querySelectorAll('.nav-item');
 const burger = document.querySelector('.icon');
 
 let navState = "closed";
 
 nameTitle.addEventListener('click', startLoader);
 burger.addEventListener('click', toggleNav);
+
+for (const navItem of navItems) {
+    navItem.addEventListener('click', function(event) {
+        nav.classList.add('invisible');
+    })
+  }
 
 function startLoader(){
     const loadWebsite = setTimeout(startWebsite, 3000);
