be163dce62847470080d8fdf68930f651dc1ab38
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2023-04-12 21:21:10 +0200

    added T1H students

---
+++ bootstrap/js/main.js
@@ -0,0 +1,110 @@
+let studentsT1H = [
+    {
+      name:"Koen Aakster",
+      profilePic:"",
+      gitHubUsername:"Oranos-Dragon",
+      gitHubLink:"Oranos-Dragon.github.io"
+    },
+    {
+      name:"Jens Heijer",
+      profilePic:"",
+      gitHubUsername:"jensdh",
+      gitHubLink:"jensdh.github.io"
+    },
+    {
+      name:"Phoebe Groeneweg",
+      profilePic:"",
+      gitHubUsername:"phoebemeow",
+      gitHubLink:"phoebemeow.github.io"
+    },
+    {
+      name:"Harmanpreet Kaur",
+      profilePic:"",
+      gitHubUsername:"Harmanpreet21",
+      gitHubLink:"Harmanpreet21.github.io"
+    },
+    {
+      name:"Rafael Nunes Coelho",
+      profilePic:"",
+      gitHubUsername:"rafaelnun3s",
+      gitHubLink:"rafaelnun3s.github.io"
+    },
+    {
+      name:"Pawanpreet Singh",
+      profilePic:"",
+      gitHubUsername:"",
+      gitHubLink:""
+    },
+    {
+      name:"Martijn Barel",
+      profilePic:"",
+      gitHubUsername:"mabarel",
+      gitHubLink:"mabarel.github.io"
+    },
+    {
+      name:"Olaf Krypczyk",
+      profilePic:"",
+      gitHubUsername:"jrasb",
+      gitHubLink:"jrasb.github.io"
+    },
+    {
+      name:"Kuray Baş",
+      profilePic:"",
+      gitHubUsername:"",
+      gitHubLink:""
+    },
+    {
+      name:"Alexi Delidis",
+      profilePic:"",
+      gitHubUsername:"",
+      gitHubLink:""
+    },
+    {
+      name:"Kevin Aarts",
+      profilePic:"",
+      gitHubUsername:"kevin-aarts",
+      gitHubLink:"kevin-aarts.github.io"
+    },
+    {
+      name:"Marnix Verstappen",
+      profilePic:"",
+      gitHubUsername:"MarnixVerstappen",
+      gitHubLink:"MarnixVerstappen.github.io"
+    },
+    {
+      name:"Nasim Saaïdi",
+      profilePic:"",
+      gitHubUsername:"nasim-saaidi",
+      gitHubLink:"nasim-saaidi.github.io"
+    },
+    {
+      name:"Tony Özbey",
+      profilePic:"",
+      gitHubUsername:"t-ozbey",
+      gitHubLink:"t-ozbey.github.io"
+    },
+    {
+      name:"Arjun Santokhi",
+      profilePic:"",
+      gitHubUsername:"",
+      gitHubLink:""
+    },
+    {
+      name:"Mamadou Bah",
+      profilePic:"",
+      gitHubUsername:"",
+      gitHubLink:""
+    },
+    {
+      name:"Dean Cheung Yue Loi",
+      profilePic:"",
+      gitHubUsername:"d-cyl",
+      gitHubLink:"d-cyl.github.io"
+    },
+    {
+      name:"Ilias Bousair",
+      profilePic:"",
+      gitHubUsername:"IliasBousair",
+      gitHubLink:"IliasBousair.github.io"
+    }
+  ]
