ad70d9f4e4beb29ddcbbf4785119952151998df9
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2024-06-28 00:46:35 +0200

    progressbar added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const navLinks = document.querySelector('.nav-links');
 const navMenu = document.querySelector('.nav-menu');
+const progressBar = document.querySelector(".progress-bar");
+const totalHeight = document.body.scrollHeight - window.innerHeight;
 
 navMenu.addEventListener('click', onToggleMenu);
 
 function onToggleMenu() {
-    console.log("WAAROMMMM");
+    console.log("hoi :)");
     navLinks.classList.toggle('top-[8vh]');
 }
+
+window.onscroll = function () {
+  const progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progressBar.style.width = progressHeight + "%";
+};
