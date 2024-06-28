657d3c1c69505a0a49540693e6b00933e238391f
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2024-06-28 22:02:13 +0200

    dropdown menu added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 const navLinks = document.querySelector('.nav-links');
 const navMenu = document.querySelector('.nav-menu');
 const progressBar = document.querySelector(".progress-bar");
+const otherVersionBtn = document.querySelector('.other-version-btn');
+const verionList = document.querySelector('.version-list');
 const totalHeight = document.body.scrollHeight - window.innerHeight;
 
 navMenu.addEventListener('click', onToggleMenu);
+otherVersionBtn.addEventListener('click', openVersionList);
 
 function onToggleMenu() {
-    console.log("hoi :)");
     navLinks.classList.toggle('top-[8vh]');
 }
 
+function openVersionList() {
+    verionList.classList.toggle('hidden');
+    const closeList = setTimeout(closeVerionlist, 3000);
+}
+
+function closeVerionlist() {    
+    verionList.classList.toggle('hidden');
+}
+
 window.onscroll = function () {
-  const progressHeight = (window.pageYOffset / totalHeight) * 100;
-  progressBar.style.width = progressHeight + "%";
+    const progressHeight = (window.scrollY / totalHeight) * 100;
+    progressBar.style.width = progressHeight + "%";
 };
